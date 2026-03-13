bb10d56b43b85eb269fca03bd09fe2560e499f44
Diana BUYINZA <buyinzadiana@gmail.com>
2026-03-13 08:32:15 +0200

    created pacman game condition and implemented it in main class to run

--- src/main/java/COM/AppGamingBasic/example/Main.java
+++ src/main/java/COM/AppGamingBasic/example/Main.java
@@ -5,8 +5,9 @@ package COM.AppGamingBasic.example;
 public class Main {
     public static void main(String[] args) {
 
-      var game = new SuperContraGame();
+//      var game = new SuperContraGame();
 //        var game  = new SuperMario();
+        var game = new PacmanGame();
         var gameRunner = new GameRunner(game);
         gameRunner.run();
     }
--- src/main/java/COM/AppGamingBasic/example/PacmanGame.java
+++ src/main/java/COM/AppGamingBasic/example/PacmanGame.java
@@ -1,4 +1,22 @@
 package COM.AppGamingBasic.example;
 
-public class PacmanGame {
+public class PacmanGame implements GamingConsole {
+
+    public void up() {
+        System.out.println("up");
+    }
+    public void down(){
+
+        System.out.println("Go down");
+    }
+
+    public void left(){
+
+        System.out.println("Go left");
+    }
+
+    public void right(){
+        System.out.println("Go right");
+    }
+
 }
